07e385f81a9155e692b947bf9559391adc1122d1
Stefan Bischof <stbischof@bipolis.org>
2025-08-06 00:47:17 +0200

    ProbeBox Implementation and Setup
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

---
+++ application/probebox/src/main/java/org/eclipse/daanse/server/application/probebox/ProbeBox.java
@@ -0,0 +1,127 @@
+/*
+* Copyright (c) 2025 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   SmartCity Jena - initial
+*   Stefan Bischof (bipolis.org) - initial
+*/
+package org.eclipse.daanse.server.application.probebox;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.eclipse.daanse.io.fs.watcher.api.FileSystemWatcherWhiteboardConstants;
+import org.eclipse.daanse.olap.core.api.Constants;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.cm.annotations.RequireConfigurationAdmin;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.RequireServiceComponentRuntime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(immediate = true)
+@RequireConfigurationAdmin
+@RequireServiceComponentRuntime
+public class ProbeBox {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProbeBox.class);
+
+    private static final String CONFIG_IDENT = "probeBox";
+    private static final String TARGET_EXT = ".target";
+    private static final String PATH_TO_OBSERVE = "./catalogs";
+    private static final String CATALOG_PATH = Paths.get(PATH_TO_OBSERVE).toAbsolutePath().normalize().toString();
+
+    private static final String PID_MS_SOAP_MSG_SAAJ = "org.eclipse.daanse.xmla.server.jakarta.saaj.XmlaServlet";
+    private static final String PID_XMLA_SERVICE = "org.eclipse.daanse.olap.xmla.bridge.ContextGroupXmlaService";
+    private static final String PID_CONTEXT_GROUP = "org.eclipse.daanse.olap.core.BasicContextGroup";
+
+    @Reference
+    ConfigurationAdmin ca;
+
+    private Configuration configXmlaEndpoint;
+    private Configuration confSoapLoggingHandler;
+    private Configuration confContextGroupXmlaService;
+    private Configuration confDataSource;
+    private Configuration confContextGroup;
+
+    @Activate
+    public void activate() throws IOException {
+        logger.info("Activating ProbeBoxSetup");
+
+        initXmlaEndPoint();
+        initXmlaService();
+        initFileListener();
+        initContextGroup();
+
+        logger.info("ProbeBoxSetup activation completed");
+    }
+
+    private void initFileListener() throws IOException {
+
+        confDataSource = ca.getFactoryConfiguration(ProbeBoxFileListener.PID, CONFIG_IDENT, "?");
+
+        Dictionary<String, Object> propsDS = new Hashtable<>();
+        propsDS.put(FileSystemWatcherWhiteboardConstants.FILESYSTEM_WATCHER_PATH, CATALOG_PATH);
+
+        confDataSource.update(propsDS);
+    }
+
+    private void initContextGroup() throws IOException {
+
+        confContextGroup = ca.getFactoryConfiguration(PID_CONTEXT_GROUP, CONFIG_IDENT, "?");
+
+        Dictionary<String, Object> propsCG = new Hashtable<>();
+        propsCG.put(Constants.BASIC_CONTEXT_GROUP_REF_NAME_CONTEXTS + TARGET_EXT, "(service.pid=*)");
+
+        confContextGroup.update(propsCG);
+    }
+
+    private void initXmlaService() throws IOException {
+
+        confContextGroupXmlaService = ca.getFactoryConfiguration(PID_XMLA_SERVICE, CONFIG_IDENT, "?");
+
+        Dictionary<String, Object> dict = new Hashtable<>();
+        dict.put("contextGroup" + TARGET_EXT, "(service.pid=*)");
+
+        confContextGroupXmlaService.update(dict);
+    }
+
+    private void initXmlaEndPoint() throws IOException {
+
+        configXmlaEndpoint = ca.getFactoryConfiguration(PID_MS_SOAP_MSG_SAAJ, CONFIG_IDENT, "?");
+
+        Dictionary<String, Object> dict = new Hashtable<>();
+        dict.put("xmlaService.target", "(service.pid=*)");
+        dict.put("osgi.http.whiteboard.servlet.pattern", "/xmla");
+
+        configXmlaEndpoint.update(dict);
+
+    }
+
+    @Deactivate
+    public void deactivate() throws IOException {
+        logger.info("Deactivating ProbeBoxSetup");
+
+        if (configXmlaEndpoint != null) {
+            configXmlaEndpoint.delete();
+        }
+        if (confSoapLoggingHandler != null) {
+            confSoapLoggingHandler.delete();
+        }
+
+        logger.info("ProbeBoxSetup deactivation completed");
+    }
+
+}
